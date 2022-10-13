ac67ccfb78442842a38619a13fa818f42380cc34
Wayskid02 <wayskid02@gmail.com>
2022-10-13 14:48:44 -0500

    Made the edit button work

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -41,32 +41,61 @@ export function FeedbackProvider({ children }) {
         {
             id: uuid(),
             rating: 5,
-            text: "Hello there woman"
+            text: "Hey, come here boy"
         },
     ]);
 
     //Submit Feedback
     function handleSubmit(e) {
         e.preventDefault()
-        setFeedbackListArray(prevArray=>{
-            return [
-                ...prevArray,
-                {
-                    id: uuid(),
-                    rating: ratingScore,
-                    text: inputVal
-                }
-            ]
-        })
+
+        const newFeedBack = {
+            id: uuid(),
+            rating: ratingScore,
+            text: inputVal
+        }
+
+        if (itemEditInfo.editStatus === true) {
+            setFeedbackListArray(feedbackListArray.map(el=>{
+                return el.id === itemEditInfo.itemEdit.id ? {
+                    ...newFeedBack
+                } : el
+            }))
+
+            setItemEditInfo({
+                itemEdit: {},
+                editStatus: false
+            })
+        } else {
+            setFeedbackListArray(prevArray=>{
+                return [
+                    ...prevArray,
+                    newFeedBack
+                ]
+            })
+        }
+
+        
         setRatingScore("10")
         setInputVal("")
     }
 
     //Edit Feedback
+    const [ itemEditInfo, setItemEditInfo ] = useState({
+        itemEdit: {},
+        editStatus: false
+    })
 
-    function handleEdit(item){
-        setInputVal(item.text)
-        setRatingScore(`${item.rating}`)
+    function handleEdit(itemToEdit){
+        setInputVal(itemToEdit.text)
+        setRatingScore(`${itemToEdit.rating}`)
+
+        setItemEditInfo({
+            itemEdit: {...itemToEdit},
+            editStatus: true
+        })
+
+        document.querySelector(".inputTextDiv>input").focus() //Make the input field focus...Don't want to use uesRef for now
     }
 
     
